50a052feb92c0cc935b9f3a000d4483e62cdcc3a
Mumeeta <45231106+Mumeeta@users.noreply.github.com>
2023-10-04 11:39:07 +0300

    added css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,18 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <div>
+      <div class="Main">
         <span>Countries app </span>is a simple React application made in
         Business College Helsinki lessons. App uses{' '}
         <a href="https://restcountries.com/">https://restcountries.com/ </a> and{' '}
         <a href="https://openweathermap.org/">https://openweathermap.org/</a>
+
+        <div class="loading" data-loading-text=" Welcome to React Advance task countries App..."></div>
+
       </div>
     </div>
+    
+    
   );
 };
 
